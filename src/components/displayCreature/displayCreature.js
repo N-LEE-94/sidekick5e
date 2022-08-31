1d3f5888634d7bc26651b60375094566b519bfdc
N-LEE-94 <nathanlee42@btinternet.com>
2022-08-31 17:37:04 +0100

    accordion now works! Next step is API calls to open5e

--- src/components/displayCreature/displayCreature.js
+++ src/components/displayCreature/displayCreature.js
@@ -1,34 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import creatureNames from "../../lib/creatureNames.js";
 import './displayCreature.css'
 
 export function DisplayCreature() {
-  
-  var acc = document.getElementsByClassName("accordion");
-  var i;
+  const [activeIndex, setActiveIndex] = useState(null);
 
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
-      this.classList.toggle("active");
-      console.log("accordion clicked")
-
-      /* Toggle between hiding and showing the active panel */
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-    });
+  function toggleActiveIndex(index) {
+    if (activeIndex === null){setActiveIndex(index)}
+      else if (activeIndex === index) {setActiveIndex(null)}
+      else if (activeIndex !==index && activeIndex !== null) {setActiveIndex(index)}
   }
 
-  return creatureNames.map((creature) => {
+  return creatureNames.map((creature, index) => {
+    const isActive = index === activeIndex;
     return (
-      <div key={creature.id}>
-        <button className="accordion" key={creature.name}>{creature.name}</button>
-        <div className="panel">
+      <div className="creatureContainer" key={creature.id}>
+        <button className="accordion" key={creature.name} onClick={() => toggleActiveIndex(index)}>
+          {creature.name}
+        </button>
+        <div className={`${isActive ? "panel" : "noShow"}`} key={index}>
           <p>{creature.name}</p>
         </div>
       </div>
